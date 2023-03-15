1c225c86672cbf681a9a6fae24c79ebe67ae4b87
Jiawei Zhang <115541164+JiaweiZhang-THU@users.noreply.github.com>
2023-03-15 15:07:14 +0800

    Add files via upload

---
+++ CAVSim_Main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <list>
+#include<math.h>
+#include<ctime>
+#include <pthread.h>
+#include <functional>
+#include <future>
+
+// #include "Simulator/simulatorSingleIntersection.h"
+// #include "Simulator/SimulatorMerging.h"
+#include "Simulator/SimulatorStraightLaneForStringStability.h"
+
+int main()
+{
+    cout << "\n\t CAVSim: a traffic simulator for connected and automated vehicles (CAVs) \n\n";
+
+    //----------------------------------- Cooperative Driving at Signal-free Intersections -----------------------------------
+    /*Simulator1 SimulatorOfIntersection;
+    SimulatorOfIntersection.SimulatorId = rand() % 10000 + 10000;
+    SimulatorOfIntersection.Run();*/
+
+    //----------------------------------- Cooperative Driving at On-Ramps ----------------------------------- 
+    /*SimulatorMerging SimulatorOfOnRamp;
+    SimulatorOfOnRamp.SimulatorId = rand() % 10000 + 20000;
+    SimulatorOfOnRamp.Run();*/
+
+    //----------------------------------- String Stability Testing of CAV Platoon ----------------------------------- 
+    SimulatorStraightLaneForStringStability SimulatorOfStraightLaneForStringStability;
+    SimulatorOfStraightLaneForStringStability.SimulatorId = rand() % 10000 + 30000;
+    SimulatorOfStraightLaneForStringStability.Run();
+
+    return 0;
+}
