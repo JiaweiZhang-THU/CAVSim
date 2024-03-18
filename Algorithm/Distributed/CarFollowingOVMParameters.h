a519addc748444c2381f71812101570ae80376f0
JiaweiZhang-THU <115541164+JiaweiZhang-THU@users.noreply.github.com>
2024-03-18 09:36:39 +0800

    Update CarFollowingOVMParameters.h

--- Algorithm/Distributed/CarFollowingOVMParameters.h
+++ Algorithm/Distributed/CarFollowingOVMParameters.h
@@ -1,11 +1,7 @@
 #pragma once
 
 double paraCarFolloingTimeStep_OVM = 0.1;
-
 double OVM_PARAMETERS_hst = 5;
-
 double OVM_PARAMETERS_hgo = 30;
-
 double OVM_PARAMETERS_alpha = 0.05;
-
 double OVM_PARAMETERS_beta = 0.02;
